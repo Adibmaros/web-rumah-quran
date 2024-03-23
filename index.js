3a3cceba1bdd4174a9aacf9bdca7d5397ce53bfa
Adibmaros <adibmaros12@gmail.com>
2024-03-23 21:07:04 +0700

    dsdfsd

---
+++ index.js
@@ -0,0 +1,12 @@
+const pendaftaran = document.getElementById('pendaftaran')
+const produk = document.getElementById('produk')
+
+
+
+pendaftaran.addEventListener('click', function(){
+    alert('pergi ke navbar di atas dan klik menu contact untuk mendapatkan informasi mengenai program dan pendaftaran')
+})
+
+produk.addEventListener('click', function(){
+    alert
+})
